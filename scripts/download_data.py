5e972441a12f75aa32157bcd1a90885799871639
HarshaSrirangam <harshasrirangam3@gmail.com>
2026-06-16 14:23:28 -0700

    removed non-emaonly from download_data.py

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 FILES = {
     "data/weights/v1-5-pruned-emaonly.safetensors": "https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors",
-    "data/weights/v1-5-pruned.safetensors": "https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned.safetensors",
 }
 
 for path, url in FILES.items():
